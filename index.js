a892c7caf1543798741cd7ad598a37a5c0b9d427
KairoBuilds <itachibeast7@gmail.com>
2026-09-13 00:39:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-
+export default {
+  async fetch(request) {
+    return new Response(
+      JSON.stringify({
+        success: true,
+        app: "KAICODE",
+        message: "KAICODE backend is online!"
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+  }
+};
